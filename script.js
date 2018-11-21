c313a8a89a2c2a5bcddc8251088adba5deb61c7e
mjablonska <martawaldon@gmail.com>
2018-11-21 11:03:58 +0100

    Added functionality to prevent double click on element

--- script.js
+++ script.js
@@ -7,19 +7,19 @@ const startTime = new Date().getTime();
 let activeItem = '';
 const activeItems = [];
 
-const gamePairs = items.length/2;
+const gamePairs = itemsLogos.length/2;
 let gameResult = 0;
 
 function clicked() {
     activeItem = this;
+    if(activeItem == activeItems[0]) //jeśli ktoś dwa razy kliknie w to samo konczymy dzialanie funkcji
+        return;
+    
     activeItem.classList.remove('hidden');
     
     if(activeItems.length === 0) { //sprawdza czy jest to pierwsze kliknięcie 
         activeItems[0] = activeItem;
-        gameResult++;
-        if(gameResult == gamePairs) { //sprawdza czy gra już jest wygrana
-            console.log('hura');
-        }
+        return;
     }
     
     else {              //sprawdza czy to drugie kliknięcie
@@ -29,24 +29,31 @@ function clicked() {
         activeItems[1] = activeItem;
         
         setTimeout(function() {//opóżnia wyświetlenie żeby można było zobaczyć kart
+            
                if(activeItems[0].className === activeItems[1].className) { //porównuje dwa kliknięte lementy czy są takie same
-            activeItems.forEach(item => {
-                item.classList.add('completed'); 
-            })
-        } else {
-            activeItems.forEach(item => {
-                item.classList.add('hidden');
-            })
-        }
-        activeItem = '';  //resetuje nam aktywne katy po zakończeniu pojedynczej gry
-        activeItems.length=0;
-            
-        items.forEach(item => {   //dodaje znów nasłuchiwacza 
-            item.addEventListener('click', clicked);
-        })    
-            
-        }, 1000)
-        
+                    activeItems.forEach(item => {
+                    item.classList.add('completed'); 
+                    })   
+                    gameResult++;
+                    if(gameResult == gamePairs) { //sprawdza czy gra już jest wygrana
+                        const endTime = new Date().getTime();
+                        const gameTime = (endTime - startTime)/1000;
+                        alert(`Wygrana! Czas gry: ${gameTime} sekund`)
+                        location.reload(); //zacznij gre                     
+                    }
+                } else {
+                    activeItems.forEach(item => {
+                        item.classList.add('hidden');
+                    })
+                }
+                activeItem = '';  //resetuje nam aktywne katy po zakończeniu pojedynczej gry
+                activeItems.length=0;
+
+                items.forEach(item => {   //dodaje znów nasłuchiwacza 
+                    item.addEventListener('click', clicked);
+                })    
+
+                }, 500)
      
     }
 }
